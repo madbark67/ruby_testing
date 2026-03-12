3dd3d2a6675a9f76486ad75635231947ebb9dded
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-03-11 22:26:28 -0400

    Complete spec exercise 14

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -114,7 +114,9 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when given invalid input as argument' do
-      xit 'returns nil' do
+      it 'returns nil' do
+        invalid_input = '25'
+        expect(game_check.verify_input(invalid_input)).to eq(nil)
       end
     end
   end
@@ -175,9 +177,15 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        incorrect_one = 'd'
+        incorrect_two = 'e'
+        correct = '5'
+        allow(game_loop).to receive(:player_input).and_return(incorrect_one, incorrect_two, correct)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
@@ -208,8 +216,9 @@ describe NumberGame do
     # Create a new instance of NumberGame, with specific values for @solution,
     # @guess, and @count
     context 'when count is 2-3' do
+      subject(:game) { NumberGame.new(5, '5', 3) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
         congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
         expect { game.final_message }.to output(congrats_phrase).to_stdout
       end
@@ -219,8 +228,12 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when count is 4 and over' do
+      subject(:game) { NumberGame.new(5, '5', 4) }
+
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        final_phrase = "That was hard. It took you 4 guesses!\n"
+        expect { game.final_message }.to output(final_phrase).to_stdout
       end
     end
   end
